6a3671b4f734492bb824173b90c71b9f528f3fcf
Lucas Janzen <lucas.janzen1@marist.edu>
2018-09-11 17:46:10 -0400

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,11 @@
+#madlib.py
+#This program will replicate the mad libs books
+def main():
+    print("For this program you will enter a noun, verb, adjective, and a place")
+    noun = input("Noun: ")
+    verb = input("Verb: ")
+    adjective = input("Adjective: ")
+    place = input("Place: ")
+    print("Take your", adjective, noun, "and", verb, "it at the", place, "!")
+
+main()
