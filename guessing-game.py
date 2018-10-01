ef0c070c01f48fd181ddd8ee78f9bdf245c37297
Lucas Janzen <lucas.janzen1@marist.edu>
2018-10-01 17:42:28 -0400

    Usability improvements to the guessing game, resolves #2 and #3.

--- guessing-game.py
+++ guessing-game.py
@@ -1,13 +1,17 @@
 #guessing-game.py
 animal = 'dog'
-while animal != 'animal':
+end = 'quit'
+while animal != 'guess':
     print('The program is thinking of an animal. Please try to guess the name of the animal')
-    animal = input()
-    if animal != 'dog':
-        print("Sorry that is the wrong animal, please try again")
+    guess = input()
+    if (guess.lower() != animal and guess.lower() != end) :
+        print("Sorry that is the wrong animal, please try again. If you would like to give up type quit when it asks for the animal name.")
+    elif guess.lower() == end:
+        break
     else:
         print('Yes, that is the animal I was thinking of. Congratulations!')
         break
+
     
 
     
