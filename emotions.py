2a92100c2cc3efdcfaf86694aa238f4cf4f40785
Lucas Janzen <lucas.janzen1@marist.edu>
2018-11-02 18:57:06 -0400

    added the curremotion, the intro, and the main function

--- emotions.py
+++ emotions.py
@@ -5,7 +5,7 @@
 emotions = ["anger","happiness","disgust","fear","sadness","surprise"]
 interactions = ["reward","punish","threaten","joke"]
 
-emotion_matrix = [[1,0,0,5][5,4,3,1],[2,2,3,2],[5,3,3,5],[1,0,3,1],[5,0,3,1]]
+emotion_matrix = [[1,0,0,5],[5,4,3,1],[2,2,3,2],[5,3,3,5],[1,0,3,1],[5,0,3,1]]
 
 expressions = ["I am so angry!",
                "I'm over the moon with happiness!",
@@ -14,20 +14,35 @@ expressions = ["I am so angry!",
                "I am so sad I might cry",
                "I am so surprised!"]
 
+def intro(emotions,currEmotion):
+    print("I am feeling ", currEmotion)
+    print("Interact with me and I might change how I'm feeling")
 
 # Returns: an integer indicating one of reward, punish, joke, or threaten
-def getInteraction():
-    interaction = interactions.index(input("Please enter an interaction(reward, punish, threaten, or joke): ")
-    return (interaction)
+def getInteraction(interactions):
+    interaction = interactions.index(input("Please enter an interaction(reward, punish, threaten, or joke): "))
+    return(interaction)
+                                     
 # Based on a given emotion and action, determine the next emotional state
 # Params:
 # currEmotion - a current emotion
 # userAction - a user interaction
 # Returns: an emotion
-def lookupEmotion(currEmotion, userAction):
+def lookupEmotion(currEmotion, userAction,emotions,emotion_matrix):
     new_emotion = emotions[emotion_matrix[emotions.index(currEmotion)][userAction]]
-    return 0 # return an integer corresponding to an emotion
+    return(new_emotion) # return an integer corresponding to an emotion
 
 def showEmotion(currEmotion,expressions):
     print(expressions[currEmotion])
+
+def main():
+    currEmotion = 'anger'
+    intro(emotions,currEmotion)
+    while 1:
+        currEmotion = lookupEmotion(currEmotion,getInteraction(interactions), emotions, emotion_matrix)
+        showEmotion(emotions.index(currEmotion),expressions)
+
+main()
+    
+
 
