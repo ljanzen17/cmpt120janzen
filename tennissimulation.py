034367ddf98ee59cef294f6dadfef0833b2c50b6
Lucas Janzen <lucas.janzen1@marist.edu>
2018-11-16 18:22:05 -0500

    Finished tennis simulation

---
+++ tennissimulation.py
@@ -0,0 +1,28 @@
+# tennissimulation.py
+
+from simstats import *
+from tennismatch import *
+
+class TennisSimulation:
+    def __init__(self, probA, probB, n):
+        self.probA = probA
+        self.probB = probB
+        self.n = n
+
+    def run(self):
+        stats = SimStats()
+        for i in range(self.n):
+            match = TennisMatch(self.probA, self.probB)
+            match.play()
+
+            stats.update(match)
+            stats.printReport()
+
+
+def main():
+    ts = TennisSimulation(.6,.4,50)
+    ts.run()
+
+
+
+main()
